37aa3e19be90ef34429f34651adfab94d7e264c7
Martin Wichmann <martinw@ichmann.de>
2013-01-28 09:21:01 +0100

    Added Bachelor Thesis

---
+++ Bachelor_Thesis/images/HAL_Dir_Map.mm
@@ -0,0 +1,28 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1294408228568" ID="ID_1272875228" MODIFIED="1294408316686" TEXT="AT91RM9200">
+<node CREATED="1294408281348" ID="ID_1385526055" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/at91rm9200/v3_0/" MODIFIED="1294408285628" POSITION="right" TEXT="v3_0">
+<node CREATED="1294408281368" ID="ID_620344712" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/at91rm9200/v3_0/include/" MODIFIED="1294408287534" TEXT="include">
+<node CREATED="1294408281371" ID="ID_339313986" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/at91rm9200/v3_0/include/pkgconf/" MODIFIED="1294408311490" TEXT="pkgconf">
+<node CREATED="1294408281374" ID="ID_632171479" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/at91rm9200/v3_0/include/pkgconf/mlt_arm_at91rm9200_ram.ldi" MODIFIED="1294408281376" TEXT="mlt_arm_at91rm9200_ram.ldi"/>
+<node CREATED="1294408281390" ID="ID_1872121718" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/at91rm9200/v3_0/include/pkgconf/mlt_arm_at91rm9200_ram.h" MODIFIED="1294408281392" TEXT="mlt_arm_at91rm9200_ram.h"/>
+<node CREATED="1294408281393" ID="ID_770700506" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/at91rm9200/v3_0/include/pkgconf/mlt_arm_at91rm9200_rom.h" MODIFIED="1294408281398" TEXT="mlt_arm_at91rm9200_rom.h"/>
+<node CREATED="1294408281399" ID="ID_1672848261" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/at91rm9200/v3_0/include/pkgconf/mlt_arm_at91rm9200_rom.ldi" MODIFIED="1294408281401" TEXT="mlt_arm_at91rm9200_rom.ldi"/>
+</node>
+<node CREATED="1294408281405" ID="ID_231274094" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/at91rm9200/v3_0/include/hal_platform_setup.h" MODIFIED="1294408281407" TEXT="hal_platform_setup.h"/>
+<node CREATED="1294408281408" ID="ID_1992411904" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/at91rm9200/v3_0/include/hal_platform_ints.h" MODIFIED="1294408281410" TEXT="hal_platform_ints.h"/>
+<node CREATED="1294408281412" ID="ID_724286175" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/at91rm9200/v3_0/include/plf_io.h" MODIFIED="1294408281413" TEXT="plf_io.h"/>
+</node>
+<node CREATED="1294408281416" ID="ID_1098700761" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/at91rm9200/v3_0/cdl/" MODIFIED="1294408288161" TEXT="cdl">
+<node CREATED="1294408281421" ID="ID_277428100" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/at91rm9200/v3_0/cdl/hal_arm_at91rm9200.cdl" MODIFIED="1294408281423" TEXT="hal_arm_at91rm9200.cdl"/>
+</node>
+<node CREATED="1294408281425" ID="ID_1985513624" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/at91rm9200/v3_0/src/" MODIFIED="1294408288818" TEXT="src">
+<node CREATED="1294408281429" ID="ID_785790463" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/at91rm9200/v3_0/src/at91rm9200_misc.c" MODIFIED="1294408281431" TEXT="at91rm9200_misc.c"/>
+</node>
+<node CREATED="1294408281434" ID="ID_71680055" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/at91rm9200/v3_0/misc/" MODIFIED="1294408289192" TEXT="misc">
+<node CREATED="1294408281439" ID="ID_1335200791" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/at91rm9200/v3_0/misc/redboot_RAM.ecm" MODIFIED="1294408281444" TEXT="redboot_RAM.ecm"/>
+<node CREATED="1294408281446" ID="ID_1400526351" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/at91rm9200/v3_0/misc/redboot_ROM.ecm" MODIFIED="1294408281450" TEXT="redboot_ROM.ecm"/>
+</node>
+</node>
+</node>
+</map>
---
+++ Bachelor_Thesis/images/HAL_Var_Dir_Map.mm
@@ -0,0 +1,27 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1295601968794" ID="ID_1815627536" MODIFIED="1295602135724" TEXT="var">
+<node CREATED="1295602095747" ID="ID_763853277" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/var/v3_0/" MODIFIED="1295602101523" POSITION="right" TEXT="v3_0">
+<node CREATED="1295602095773" ID="ID_1062788858" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/var/v3_0/include/" MODIFIED="1295602102100" TEXT="include">
+<node CREATED="1295602095810" ID="ID_45469914" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/var/v3_0/include/var_arch.h" MODIFIED="1295602095812" TEXT="var_arch.h"/>
+<node CREATED="1295602095836" ID="ID_1302040217" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/var/v3_0/include/hal_diag.h" MODIFIED="1295602095840" TEXT="hal_diag.h"/>
+<node CREATED="1295602095842" ID="ID_1077506158" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/var/v3_0/include/var_io.h" MODIFIED="1295602095845" TEXT="var_io.h"/>
+<node CREATED="1295602095847" ID="ID_1972661957" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/var/v3_0/include/hal_cache.h" MODIFIED="1295602095849" TEXT="hal_cache.h"/>
+<node CREATED="1295602095850" ID="ID_1431939008" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/var/v3_0/include/plf_stub.h" MODIFIED="1295602095852" TEXT="plf_stub.h"/>
+</node>
+<node CREATED="1295602095855" ID="ID_1911546834" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/var/v3_0/cdl/" MODIFIED="1295602103039" TEXT="cdl">
+<node CREATED="1295602095859" ID="ID_907281170" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/var/v3_0/cdl/hal_arm_at91.cdl" MODIFIED="1295602095860" TEXT="hal_arm_at91.cdl"/>
+</node>
+<node CREATED="1295602095862" ID="ID_324136298" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/var/v3_0/src/" MODIFIED="1295602104621" TEXT="src">
+<node CREATED="1295602095891" ID="ID_1217696151" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/var/v3_0/src/at91_misc.c" MODIFIED="1295602095893" TEXT="at91_misc.c"/>
+<node CREATED="1295602095894" ID="ID_1691796994" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/var/v3_0/src/hal_diag_dcc.h" MODIFIED="1295602095895" TEXT="hal_diag_dcc.h"/>
+<node CREATED="1295602095896" ID="ID_1266245706" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/var/v3_0/src/timer_pit.c" MODIFIED="1295602095898" TEXT="timer_pit.c"/>
+<node CREATED="1295602095900" ID="ID_1668044998" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/var/v3_0/src/timer_tc.c" MODIFIED="1295602095902" TEXT="timer_tc.c"/>
+<node CREATED="1295602095903" ID="ID_263904449" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/var/v3_0/src/hal_diag.c" MODIFIED="1295602095904" TEXT="hal_diag.c"/>
+<node CREATED="1295602095916" ID="ID_1464677242" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/var/v3_0/src/hal_diag_dbg.c" MODIFIED="1295602095919" TEXT="hal_diag_dbg.c"/>
+<node CREATED="1295602095929" ID="ID_294404151" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/var/v3_0/src/hal_diag_dcc.c" MODIFIED="1295602095931" TEXT="hal_diag_dcc.c"/>
+</node>
+<node CREATED="1295602095933" ID="ID_1184377496" LINK="file:/home/erebos/xil/ecos/ecos/packages/hal/arm/at91/var/v3_0/ChangeLog" MODIFIED="1295602095936" TEXT="ChangeLog"/>
+</node>
+</node>
+</map>
